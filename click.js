c346c9cd9bce4881c722aafe7058150015950c0c
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-02-26 00:26:42 +0900

    link and add click-js

---
+++ click.js
@@ -0,0 +1,13 @@
+/*const pause = document.querySelector('#click');
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    pause.classList.toggle(CLICKED_CLASS);
+}
+function init(){
+    pause.addEventListener("click", handleClick);
+}
+init();*/
+function myFunction(x) {
+    x.classList.toggle("fa-play");
+  }
--- searchbar.js
+++ searchbar.js
@@ -2,3 +2,4 @@ $('#toggle-search').on('click', function() {
   $('#searchBar').toggle('display: inline-block');
   $('#hide-bar').toggle('visibility: hidden');
 });
+
