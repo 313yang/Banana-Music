ffbd6575bc4679dfe0ea130d9da05a1e782a4d9d
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-03-01 17:37:23 +0900

    add chart, like, playlist

--- js/click.js
+++ js/click.js
@@ -1,13 +1,8 @@
-/*const pause = document.querySelector('#click');
-const CLICKED_CLASS = "clicked";
 
-function handleClick() {
-    pause.classList.toggle(CLICKED_CLASS);
-}
-function init(){
-    pause.addEventListener("click", handleClick);
-}
-init();*/
 function myFunction(x) {
     x.classList.toggle("fa-play");
+  }
+
+  function myFunction(heart){
+      heart.classList.toggle("fa-heart");
   }
